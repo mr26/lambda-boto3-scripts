d182cf5188ecdc5d8e3ad5210fdfd3548657a9a7
mehdi <mehdir26@gmail.com>
2019-09-30 16:14:59 -0400

    added lockdown script

---
+++ lockdown_public_buckets.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3.6
+
+import boto3
+
+session = boto3.session.Session(profile_name='mehdir26aws')
+
+s3 = session.resource('s3')
+
+client = session.client('s3')
+
+
+def lockdown_pub_buckets():
+
+    for bucket in s3.buckets.all():
+        bucket_access = s3.BucketAcl(bucket.name).grants
+
+        for i in range(0,len(bucket_access)):
+            try:
+                if bucket_access[i]['Grantee']['URI'] == 'http://acs.amazonaws.com/groups/global/AllUsers':
+                    response = client.put_public_access_block(
+                            Bucket= bucket.name,
+                            PublicAccessBlockConfiguration={
+                                'BlockPublicAcls': True,
+                                'BlockPublicPolicy': True,
+                                'RestrictPublicBuckets': True,
+                                'IgnorePublicAcls': True
+                                }
+                            )
+            except KeyError:
+                pass
+
+
+if __name__ == '__main__':
+    lockdown_pub_buckets()
+
